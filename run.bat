ddbfa0270e50af7d7dcdcf0eda1e5be714ce47e8
Christos Miniotis <chrismin1302@gmail.com>
2022-02-10 16:07:37 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,2 @@
- :: DO NOT USE RELATIVE PATHS
- curl https://raw.githubusercontent.com/chrismin13/parsec-download-script/main/script.bat -o script.bat %temp%
- %temp%\script.bat && del %temp%\script.bat
+curl https://raw.githubusercontent.com/chrismin13/parsec-download-script/main/script.bat -o "%temp%\script.bat"
+"%temp%\script.bat" && del %temp%\script.bat
